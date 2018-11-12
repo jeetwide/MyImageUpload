3434655bf539d16041d07b1757ef1346aeb87f84
jeetwide <wide.angle456@gmail.com>
2018-11-12 12:11:24 +0530

    Initial commit

---
+++ app/src/main/java/my/image/upload/Api.java
@@ -0,0 +1,25 @@
+package my.image.upload;
+
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+/**
+ * Created by NIK on 12-11-2018.
+ */
+public interface Api {
+
+    //the base URL for our API
+    //make sure you are not using localhost
+    //find the ip usinc ipconfig command
+    String BASE_URL = "http://192.168.1.18:8080/user/";
+
+    //this is our multipart request
+    //we have two parameters on is name and other one is description
+    @Multipart
+    @POST("fileUpload")
+    Call<MyResponse> uploadImage(@Part("image\"; filename=\"myfile.jpg\" ") RequestBody file/*, @Part("desc") RequestBody desc*/);
+
+}
---
+++ app/src/main/java/my/image/upload/MyResponse.java
@@ -0,0 +1,13 @@
+package my.image.upload;
+
+/**
+ * Created by NIK on 12-11-2018.
+ */
+
+public class MyResponse {
+
+
+       // boolean error;
+        String message;
+
+}
